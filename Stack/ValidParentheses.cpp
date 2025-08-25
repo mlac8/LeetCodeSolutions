5ec85e0217e69f5b9b5b386a7f289ae4f0eca1bc
Mark Lacanilao <mlac8@outlook.com>
2025-08-25 12:26:44 -0400

    added valid parentheses

--- Stack/ValidParentheses.cpp
+++ Stack/ValidParentheses.cpp
@@ -1,3 +1,25 @@
 //
 // Created by Mark Lacanilao on 8/8/25.
+class Solution {
+public:
+    bool isValid(string s) {
+        while (true) {
+            size_t pos = string::npos;
+            if ((pos = s.find("()")) != string::npos) {
+                s.erase(pos, 2);
+                continue;
+            }
+            if ((pos = s.find("{}")) != string::npos) {
+                s.erase(pos, 2);
+                continue;
+            }
+            if ((pos = s.find("[]")) != string::npos) {
+                s.erase(pos, 2);
+                continue;
+            }
+            break;
+        }
+        return s.empty();
+    }
+};
 //
